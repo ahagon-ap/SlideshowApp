ec4730868334b7a6dad77fc586095e21b55c5763
Ahagon-ap <s.ahagon@alphapolis.co.jp>
2024-07-02 11:08:44 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by WEBSYSTEM-MAC41 on 2024/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
