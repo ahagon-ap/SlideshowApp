5d63ef34f146ba64d3fdf07a81071e03964e88eb
Ahagon-ap <s.ahagon@alphapolis.co.jp>
2024-07-04 14:22:00 +0900

    整形とコメント修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -1,14 +1,12 @@
 //
 //  ViewController.swift
 //  SlideshowApp
-//
-//  Created by WEBSYSTEM-MAC41 on 2024/07/02.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    // UI部品との接続
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var goButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
@@ -20,10 +18,6 @@ class ViewController: UIViewController {
     // スライドショーに使用するタイマーを宣言
     var timer: Timer!
     
-    var go: String = "plus"
-    
-    var back: String = "minus"
-    
     // スライドショーさせる画像の配列を宣言
     var imageArray:[UIImage] = [
         UIImage(named: "dog1.jpg")!,
@@ -34,7 +28,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 最初に使用する画像の説明
         let firstImage = UIImage(named: "dog1.jpg")
         dogImages.image = firstImage
         dogImages.isUserInteractionEnabled = true
@@ -42,84 +36,92 @@ class ViewController: UIViewController {
         dogImages.addGestureRecognizer(tapGestureRecognizer)
     }
     
+    //  画像がタップされた時の関数
     @objc func imageViewTapped(_ sender: UITapGestureRecognizer) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                if let detailVC = storyboard.instantiateViewController(withIdentifier: "ZoomImageViewController") as? ZoomImageViewController {
-                    detailVC.image = dogImages.image
-                    self.present(detailVC, animated: true, completion: nil)
-                }
+        // 2つ目の画面にタップした画像のデータを送る
+        if let detailVC = storyboard.instantiateViewController(withIdentifier: "ZoomImageViewController") as? ZoomImageViewController {
+            detailVC.image = dogImages.image
+            self.present(detailVC, animated: true, completion: nil)
+        }
     }
     
+    // 再生ボタンを押した時の関数
     @IBAction func start(_ sender: Any) {
         // 再生中か停止しているかを判定
         if (timer == nil) {
-            // 再生時の処理を実装
-            // タイマーをセットする
+            // 再生時の処理
             timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImageAutomatically), userInfo: nil, repeats: true)
             
             // ボタンの名前を停止に変える
             startButton.setTitle("停止", for: .normal)
-            
+            // ボタンを操作不可に
             goButton.isEnabled = false
             backButton.isEnabled = false
             
         } else {
             // 停止時の処理を実装
-            // タイマーを停止する
             timer.invalidate()
             
-            // タイマーを削除しておく(timer.invalidateだけだとtimerがnilにならないため)
+            // タイマーの削除
             timer = nil
             
             // ボタンの名前を再生に変える
             startButton.setTitle("再生", for: .normal)
-            
+            // ボタンを操作可能に
             goButton.isEnabled = true
             backButton.isEnabled = true
             
         }
     }
     
+    // 進むボタン
     @IBAction func go(_ sender: Any) {
         changeImage(direction: "plus")
     }
     
+    // 戻るボタン
     @IBAction func back(_ sender: Any) {
         changeImage(direction: "minus")
     }
     
+    // セレクタに入れるため
     @objc func changeImageAutomatically() {
         changeImage(direction: "plus")
     }
     
+    // 画像の遷移する順番
     @objc func changeImage(direction: String) {
         
+        // 再生ボタンと進むボタンの時の挙動
         if (direction == "plus"){
             
             // indexを増やして表示する画像を切り替える
             nowIndex += 1
-            
             // indexが表示予定の画像の数と同じ場合
             if (nowIndex == imageArray.count) {
                 // indexを一番最初の数字に戻す
                 nowIndex = 0
             }
+            
         } else {
             
             // indexを増やして表示する画像を切り替える
             nowIndex -= 1
-            
             // indexが表示予定の画像の数と同じ場合
             if (nowIndex < 0) {
                 // indexを一番最初の数字に戻す
                 nowIndex = imageArray.count - 1
             }
         }
+        
+        // 表示する画像の確定
         dogImages.image = imageArray[nowIndex]
     }
     
+    // 2つ目の画像の戻るボタン用
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
-        }
+    }
     
 }
 
--- SlideshowApp/ZoomImageViewController.swift
+++ SlideshowApp/ZoomImageViewController.swift
@@ -1,35 +1,24 @@
 //
 //  ZoomImageViewController.swift
 //  SlideshowApp
-//
-//  Created by WEBSYSTEM-MAC41 on 2024/07/03.
-//
 
 import UIKit
 
 class ZoomImageViewController: UIViewController {
 
+    // UI部品との接続
     @IBOutlet weak var DetailImage: UIScrollView!
     
+    // UIimageの変数定義
     var image: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let widthSize = UIScreen.main.bounds.size.width // 画面の横の大きさを取得
-        let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: widthSize, height: widthSize * 1.2)) // 背景画像の大きさを設定
-        imageViewBackground.image = image // 画像を設定
-        self.view.addSubview(imageViewBackground) // 背景画像を追加する
+        // 画面の横の大きさを取得
+        let widthSize = UIScreen.main.bounds.size.width
+        // 拡大画像の大きさを設定(横は画面の大きさに縦は横の大きさの1.2倍)
+        let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: widthSize, height: widthSize * 1.2))
+        imageViewBackground.image = image
+        self.view.addSubview(imageViewBackground)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
